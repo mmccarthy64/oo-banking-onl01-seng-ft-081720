7a712a68e114967ce4476fcc0ece46dc142ec1f5
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-03 20:49:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,6 +24,7 @@ class Transfer
     if @sender.balance < @amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
+      
     elsif @status == "complete"
       puts "Transaction was already excuted"
     else
