11440817e1d23b9ebefbfc90a444ef97c4c77fde
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-03 20:38:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,8 +30,8 @@ class Transfer
   
   def reverse_transfer
     if @status == 'complete'
-      @sender.deposit(@amount * -1)
-      @receiver.deposit(@amount)
+      @sender.deposit(@amount)
+      @receiver.deposit(@amount )
       @status = "reversed"
   end
 end
