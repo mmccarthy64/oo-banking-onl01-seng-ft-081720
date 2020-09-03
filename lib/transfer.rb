21b7bfd3bfe474f555944c983e19e5243023ec7f
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-03 21:01:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   end
   
 def execute_transaction
-  if @sender.balance > @amount && @status == "pending" && sender.valid?
+  if @sender.balance > @amount && @status == "pending" && self.valid?
     @sender.balance -= @amount
     @receiver.balance += @amount
     @status = "complete"
